40273ed25f45bc510d1b97746e1f228c4f79579c
Charles "CJ" Emerson <cjemerson@alaska.edu>
2019-03-21 22:04:49 -0800

    Add Circle Class Tests
     - Added the TestShape class to allow the tester to evaluate a Shape
       at a known center
     - Added the TestCircle class to allow the tester to evaluate a Circle
       at a known center
     - Added a run-one-test function for the Circle class for:
         * Bounding Box
     - Added a run-one-test function for the TestCircle class for:
         * PostScript Generation
     - Added TEST_CASEs for Basic Shapes:
         * Bounding Box
         * PostScript Generation

--- tests/cps_test_suites.cpp
+++ tests/cps_test_suites.cpp
@@ -38,6 +38,46 @@
 
 
 // *********************************************************************
+// Helper Classes for this Test Program
+// *********************************************************************
+
+// TestShape
+// Inherited to set the center of a Shape's evaluate().
+class TestShape
+{
+public:
+	TestShape()
+		: _center{0.0, 0.0}
+	{ }
+
+	virtual ~TestShape() = default;
+
+public:
+	void setCenter(point_t center)
+	{
+		_center = center;
+	}
+
+protected:
+	point_t _center;
+};
+
+// TestCircle
+// Overrides evaluate to only spit out generate() at a known center. 
+class TestCircle : public Circle, public TestShape
+{
+public:
+	TestCircle(double radius)
+		: Circle(radius), TestShape()
+	{ }
+
+	std::string evaluate() const override
+	{
+		return generate(_center);
+	}
+
+
+// *********************************************************************
 // Helper Functions for this Test Program
 // *********************************************************************
 
@@ -48,14 +88,66 @@
 // Test Functions for this Test Program
 // *********************************************************************
 
-// Nothing here currently...
+void test_circleBoundingBox(double radius)
+{
+	Circle circle(radius);
+
+	INFO("A Circle with radius " << radius << " yields a bounding box with (" << 2 * radius << ", " << 2 * radius << ")");
+	REQUIRE( circle.getBoundingBox().x == 2 * radius );
+	REQUIRE( circle.getBoundingBox().y == 2 * radius );
+}
+
+void test_circleGenerate(point_t center, double radius)
+{
+	using std::to_string;
+
+	TestCircle circle(radius);
+	circle.setCenter(center);
+
+	INFO("Circle with radius " << radius << "at (" << center.x << ", " << center.y << ") generates correct PostScript");
+	REQUIRE( circle.evaluate() == to_string(center.x) + " " + to_string(center.y) + " " + to_string(radius) + " circle\n" );
+}
 
 
 // *********************************************************************
 // Test Cases for this Test Program
 // *********************************************************************
 
-TEST_CASE( "@TODO", "[@TODO]" )
+TEST_CASE( "Basic Shapes - Bounding Box ", "[BasicShapes][BoundingBox]")
 {
-	// @TODO
+	SECTION("Circle - Bounding Box")
+	{
+		test_circleBoundingBox(0.25);
+		test_circleBoundingBox(1.0);
+		test_circleBoundingBox(50.0);
+		test_circleBoundingBox(100.0);
+		test_circleBoundingBox(500.0);
+	}
+}
+
+TEST_CASE( "Basic Shapes - PostScript Generation", "[BasicShapes][PostScript]" )
+{
+	SECTION("Circle - PostScript Generation")
+	{
+		point_t center = {0.0, 0.0};
+		test_circleGenerate(center, 0.25);
+		test_circleGenerate(center, 1.0);
+		test_circleGenerate(center, 50.0);
+		test_circleGenerate(center, 100.0);
+		test_circleGenerate(center, 500.0);
+
+		center = {500.0, 500.0};
+		test_circleGenerate(center, 0.25);
+		test_circleGenerate(center, 1.0);
+		test_circleGenerate(center, 50.0);
+		test_circleGenerate(center, 100.0);
+		test_circleGenerate(center, 500.0);
+
+		center = {-73.07333, 499.02001};
+		test_circleGenerate(center, 0.25);
+		test_circleGenerate(center, 1.0);
+		test_circleGenerate(center, 50.0);
+		test_circleGenerate(center, 100.0);
+		test_circleGenerate(center, 500.0);
+	}
 }

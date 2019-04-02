e3f6c2a4ad2fa1e310a0b17b5a43af715f9a3252
Charles "CJ" Emerson <cjemerson@alaska.edu>
2019-04-01 19:34:10 -0800

    Fix: CompoundShape.cpp Comments and Newlines
     - Added Ellipse class comment
     - Added spacing to make the Ellipse constructor more readable

--- source/CompoundShapes.cpp
+++ source/CompoundShapes.cpp
@@ -246,7 +246,14 @@ std::string Horizontal::generatePostScript(point_t center) const
 	return output;
 }
 
+
+// *********************************************************************
+// Ellipse Class
+// *********************************************************************
+
 Ellipse::Ellipse(double xRadius, double yRadius)
-: Scaled(_circle, std::min(xRadius/yRadius, 1.0), std::min(yRadius/xRadius, 1.0)),
-  _circle(std::min(xRadius, yRadius))
+    : Scaled(_circle,
+             std::min(xRadius/yRadius, 1.0),
+             std::min(yRadius/xRadius, 1.0)),
+      _circle(std::min(xRadius, yRadius))
 { }
